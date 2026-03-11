39e43a5e9e86998beb4f6bc6bf82c027035bcf32
iv <ivkushlak@gmail.com>
2026-03-11 15:44:16 +0700

    убрал лишнее

--- FileState.h
+++ FileState.h
@@ -1,4 +1,16 @@
 #ifndef FILESTATE_H
 #define FILESTATE_H
 
+#include <QString> //для пути к файлу
+#include <QtGlobal> //для размера файла
+
+struct FileState
+{
+    QString way; //путь к файлу
+
+    bool exist = false; //есть ли файл
+
+    qint64 size = 0; //его размер
+};
+
 #endif // FILESTATE_H
